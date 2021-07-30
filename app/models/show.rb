c77a6a5abb5bbc85505a375f3d0338981f560173
Antonio Clavijo <littletonito@gmail.com>
2021-07-30 09:04:22 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    # binding.pry
+    self.actors.collect do |name|
+      name.full_name
+    end
+  end
+
 end
